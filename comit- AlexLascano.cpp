c4cf23309be3644b30cce2706a965d2d60919535
AlexLascano <alexlascano151@gmail.com>
2026-01-10 18:11:13 -0500

    Commit- AlexLascano.cpp

--- comit- AlexLascano.cpp
+++ comit- AlexLascano.cpp
@@ -1,18 +1,33 @@
 
-void mostrarPeliculas(vector<Pelicula> &peliculas){
-	cout<<" ===== LISTA DE PELICULAS =====\n";
-	if(peliculas.empty()){
-		cout<<"No se encuentran peliculas registradas. ";
-	}
-	for(int i=0;i<peliculas.size();i++){
-		cout<< "--------------------\n";
-		cout<<"ID: "<< peliculas[i].Id<<endl;
-		cout<<"Titulo: "<< peliculas[i].titulo<<endl;
-		cout<<"Director: "<< peliculas[i].director<<endl;
-		cout<<"Anio: "<< peliculas[i].anio<<endl;
-		cout<<"Genero: "<< peliculas[i].genero<<endl;
-		cout<< "-----------------------\n";
-	}
-	cout <<endl;
+void mostrarPeliculas(string ruta){
+    ifstream archivo(ruta);
+
+    if(!archivo){
+        cout << "No se encuentran peliculas registradas.\n\n";
+        return;
+    }
+
+    Pelicula p;
+    cout << "===== LISTA DE PELICULAS =====\n";
+
+    while(archivo >> p.Id){
+        archivo.ignore();
+        getline(archivo, p.titulo, ';');
+        getline(archivo, p.director, ';');
+        archivo >> p.anio;
+        archivo.ignore();
+        getline(archivo, p.genero, '.');
+
+        cout << "--------------------\n";
+        cout << "ID: " << p.Id << endl;
+        cout << "Titulo: " << p.titulo << endl;
+        cout << "Director: " << p.director << endl;
+        cout << "Anio: " << p.anio << endl;
+        cout << "Genero: " << p.genero << endl;
+    }
+
+    archivo.close();
+    cout << endl;
 }
+
 
